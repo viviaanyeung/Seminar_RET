d1d3eca47c4e705e1ec1eeb2098bcbe94b14ab95
viviaanyeung <vivianyeung.1998@gmail.com>
2021-02-01 00:06:37 +0100

    Test merge

--- ShuntYard/src/Main.java
+++ ShuntYard/src/Main.java
@@ -22,6 +22,7 @@ import visualisation.ShuntDraw;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		System.out.println("Test merge Koen");
 		Instance inst = getData();
 		//Draw output
 		ArrayList<MetroMovement> movements=new ArrayList<>();
